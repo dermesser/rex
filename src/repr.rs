7ec62b308a33df12284c2098e40aa4ca31f49b7e
Lewin Bormann <lbo@spheniscida.de>
2017-08-19 06:44:43 +0000

    repr: Add optimizer module and basic test.

--- src/repr.rs
+++ src/repr.rs
@@ -277,10 +277,112 @@ impl Repetition {
     }
 }
 
+/// optimize contains functionality for optimizing the representation of regular expressions for
+/// more efficient matching.
+mod optimize {
+    use super::*;
+    use std::iter::{FromIterator, Iterator};
+    use std::ops::Deref;
+
+    pub fn optimize(mut p: Pattern) -> Pattern {
+        p = concat_chars_to_str(p);
+        p = optimize_recursively(p);
+        p
+    }
+
+    fn concat_chars_to_str(p: Pattern) -> Pattern {
+        match p {
+            Pattern::Concat(mut v) => {
+                let mut drain = v.drain(..);
+                let mut new_elems = vec![];
+
+                // Find runs of adjacent chars/strings and convert them to single strings.
+                // Once a run is broken, append non-char/string patterns and continue afterwards.
+                let mut chars = vec![];
+                for cp in &mut drain {
+                    match cp {
+                        Pattern::Char(c) => chars.push(c),
+                        Pattern::Str(mut s) => {
+                            chars.extend(s.drain(..));
+                        }
+                        e => {
+                            // Once a run of chars/strings is broken, merge the run, push the
+                            // non-char/string pattern and continue with the next one.
+                            let newp = Pattern::Str(String::from_iter(chars.drain(..)));
+                            new_elems.push(newp);
+                            new_elems.push(e);
+                            assert!(chars.is_empty());
+                        }
+                    }
+                }
+                if !chars.is_empty() {
+                    let newp = Pattern::Str(String::from_iter(chars.drain(..)));
+                    new_elems.push(newp);
+                }
+
+                if new_elems.len() == 1 {
+                    new_elems.pop().unwrap()
+                } else {
+                    Pattern::Concat(new_elems)
+                }
+            }
+            _ => p,
+        }
+    }
+
+    fn optimize_recursively(p: Pattern) -> Pattern {
+        match p {
+            Pattern::Concat(ps) => Pattern::Concat(ps.into_iter().map(optimize).collect()),
+            Pattern::Submatch(bp) => {
+                let sub = optimize(bp.deref().clone());
+                Pattern::Submatch(Box::new(sub))
+            }
+            Pattern::Alternate(ps) => Pattern::Alternate(ps.into_iter().map(optimize).collect()),
+            Pattern::Repeated(r) => {
+                let rep = r.deref().clone();
+                Pattern::Repeated(Box::new(match rep {
+                    Repetition::ZeroOrOnce(p) => Repetition::ZeroOrOnce(optimize(p)),
+                    Repetition::ZeroOrMore(p) => Repetition::ZeroOrMore(optimize(p)),
+                    Repetition::OnceOrMore(p) => Repetition::OnceOrMore(optimize(p)),
+                    Repetition::Specific(p, min, max) => {
+                        Repetition::Specific(optimize(p), min, max)
+                    }
+                }))
+            }
+
+            p => p,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use state::*;
+
+    #[test]
+    fn test_repr_optimize() {
+        // case = (want, input)
+        let case1 =
+            (Pattern::Str("abc".to_string()),
+             Pattern::Concat(vec![Pattern::Char('a'), Pattern::Char('b'), Pattern::Char('c')]));
+        let case2 = (Pattern::Str("abcd".to_string()),
+                     Pattern::Concat(vec![Pattern::Str("a".to_string()),
+                                          Pattern::Char('b'),
+                                          Pattern::Str("cd".to_string())]));
+        let case3 = (Pattern::Concat(vec![Pattern::Str("abc".to_string()),
+                                          Pattern::Anchor(AnchorLocation::End),
+                                          Pattern::Str("d".to_string())]),
+                     Pattern::Concat(vec![Pattern::Char('a'),
+                                          Pattern::Char('b'),
+                                          Pattern::Char('c'),
+                                          Pattern::Anchor(AnchorLocation::End),
+                                          Pattern::Char('d')]));
+
+        for c in vec![case1, case2, case3].into_iter() {
+            assert_eq!(c.0, optimize::optimize(c.1));
+        }
+    }
 
     // /a(b|c)/
     fn simple_re0() -> Pattern {
