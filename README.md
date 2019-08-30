8050a512154e6e28971c1d66e05307c4207388aa
Lewin Bormann <lbo@spheniscida.de>
2019-08-30 16:30:41 +0200

    Extend benchmarks

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # rex
 
-rex is a mere playground where I'm trying to write an engine for parsing, compiling and matching regular expressions.
+rex is a mere playground where I'm trying to write an engine for parsing,
+compiling and matching regular expressions.
+
+It's a bit slow right now.
 
 Consider the code licensed under the MIT license.
 
--- benches/e2e.rs
+++ benches/e2e.rs
@@ -1,14 +1,36 @@
 
 #[macro_use]
 extern crate bencher;
+extern crate regex;
 
 use bencher::Bencher;
 
 fn bench_simple_re(b: &mut Bencher) {
     b.iter(|| {
-        assert!(rex::match_re_str("(Hello)? [Ww]orld!?", "Hello world").unwrap().0);
+        assert!(rex::match_re_str("^(Hello)? [Ww]orld!?$", "Hello world").unwrap().0);
     });
 }
 
-benchmark_group!(benchs, bench_simple_re);
+fn bench_simple_precompile(b: &mut Bencher) {
+    let re = rex::compile("^(Hello)? [Ww]orld!?$").unwrap();
+    b.iter(|| {
+        assert!(rex::match_re(&re, "Hello world").0);
+    });
+}
+
+fn bench_notorious(b: &mut Bencher) {
+    let re = rex::compile("(x+x+)+y").unwrap();
+    b.iter(|| {
+        assert!(rex::match_re(&re, "xxxxxxxxxxxy").0);
+    });
+}
+
+fn bench_regex_crate(b: &mut Bencher) {
+    let re = regex::Regex::new("^(Hello)? [Ww]orld!?$").unwrap();
+    b.iter(|| {
+        assert!(re.is_match("Hello World"));
+    });
+}
+
+benchmark_group!(benchs, bench_simple_re, bench_simple_precompile, bench_notorious, bench_regex_crate);
 benchmark_main!(benchs);
